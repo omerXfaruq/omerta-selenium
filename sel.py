e2f940cbd1f3647baa61daa1d73c91e85273527e
Ömer Faruk Özdemir <farukozderim@gmail.com>
2023-08-16 16:34:36 -0400

    tweaks

--- sel.py
+++ sel.py
@@ -5,6 +5,27 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver import ActionChains, Keys
 import time
 
+def select_highest_car(driver):
+    popup_elements = driver.find_elements(By.CLASS_NAME, "popup-box-wrapper")
+    highest_probability = -1
+    selected_element = None
+
+    # Iterate through the elements and extract probabilities
+    for element in popup_elements:
+        probability_element = element.find_element(By.CLASS_NAME, "head").find_element(By.TAG_NAME, "h4")
+        probability = int(probability_element.text.strip('%'))
+        print(f"%{probability}")
+        if probability > highest_probability:
+            highest_probability = probability
+            selected_element = element
+    if selected_element is not None:
+        probability_element = selected_element.find_element(By.CLASS_NAME, "head").find_element(By.TAG_NAME, "h4")
+        probability = int(probability_element.text.strip('%'))
+        print(f"Selected: %{probability}")
+
+        button = selected_element.find_element(By.CSS_SELECTOR, '.btn.btn-red.btn-bold.btn-big')
+        driver.execute_script("arguments[0].click();", button)
+
 driver = webdriver.Chrome()
 driver.get("https://omerta.com.tr/#/jail.php")
 login_link = driver.find_element(By.XPATH, "//a[@data-bs-target='#loginModal']")
@@ -13,7 +34,7 @@ time.sleep(1)
 ActionChains(driver).send_keys(Keys.TAB).perform()
 ActionChains(driver).send_keys("fmbslayer@gmail.com").perform()
 ActionChains(driver).send_keys(Keys.TAB).perform()
-ActionChains(driver).send_keys("yo123Aa!*").perform()
+ActionChains(driver).send_keys("yo123Aa3!*").perform()
 ActionChains(driver).send_keys(Keys.RETURN).perform()
 
 time.sleep(1)
@@ -36,9 +57,7 @@ while(True):
         jail = driver.find_elements(By.XPATH, f"//*[contains(text(), '{target_text}')]")
         if jail:
             continue
-        
-        buttons = driver.find_elements(By.CSS_SELECTOR, '.btn.btn-red.btn-bold.btn-big')
-        driver.execute_script("arguments[0].click();", buttons[3])
+        select_highest_car(driver)
 
         time.sleep(28)
     except Exception as ex:
